ee5e08d1de3d88002ca316cdf2392e846f9546a2
+ <+>
2022-08-05 21:40:07 -0400

    pushing content

--- js/index.js
+++ js/index.js
@@ -5,26 +5,26 @@ const input = document.getElementById("search");
 
 function findValues (event){
     console.log("hi", "hi")
-    //     event.preventDefault();
-    //     //console.log(event.target.children[0].value)
-    //     //console.log("test")
+        event.preventDefault();
+        //console.log(event.target.children[0].value)
+        //console.log("test")
 
     
-    // console.log("input", input[0].value)
+    console.log("input", input.value)
 
-    // const value = input[0].value;
+    const value = input.value;
 
-    // fetch(`https://api.github.com/search/users?q=${value}`), {
+    fetch(`https://api.github.com/search/users?q=${value}`, {
     
-    //     headers: {
-    //         "Accept": "application/vnd.github.v3+json"
-    //     },
+        headers: {
+            "Accept": "application/vnd.github.v3+json"
+        },
     
-    // }
-    // .then((res) => res.json())
-    // .then(resp => {console.log(resp)
-    //     //resp.message.forEach (key => addResults)
-    //})
+    })
+    .then((res) => res.json())
+    .then(resp => {console.log(resp)
+        resp.items.forEach (item => addResult(item))
+    })
     }
 console.log(gitForm)
     gitForm.addEventListener("submit", findValues)
@@ -32,10 +32,9 @@ console.log(gitForm)
 function addResult(results){
     
     let newParagraph = document.createElement("p")
-    results = input[0].value
+    results = input[0].value// fix this! 
     Div.appendChild(results)
 
 }
 
 
-searchButton()
